98a6b4c2d37633a26ffe6d98d1bb31d28b920830
Nolan Miller <nolanrm20@gmail.com>
2017-01-03 09:41:44 -0600

    Updated to print best player
    Now creates player object for best at the position of choice.

--- main_scrape.py
+++ main_scrape.py
@@ -3,9 +3,9 @@ from bs4 import BeautifulSoup
 import urllib.request
 from lxml import html
 
-class RBWR:
-    def __init__(self, name, team, attempts, rushyd, rushtd, targets,
-                receptions, recyd, recTD, games_played, fpoints, fppg):
+class RB:
+    def __init__(self, name, team, games_played, attempts, rushyd, rushtd, targets,
+                receptions, recyd, recTD, fpoints, fppg):
         self.name = name
         self.team = team
         self.attempts = attempts
@@ -19,8 +19,36 @@ class RBWR:
         self.fpoints = fpoints
         self.fppg = fppg
 
-#    def print_stats(self):
+    def print_stats(self):
+        print("*------------------------------------*")
+        print("Name: \t\t", self.name, "\nTeam: \t\t", self.team, "\nTDs: \t\t", int(self.rushTD) + int(self.recTD),
+            "\nRush Yd: \t", self.rushyd, "\nRec Yd: \t", self.recyd, "\n", self.name, " scored ", self.fppg, " points per game")
+        print("* Fantasy Points {} *".format(self.fpoints))
+        print("*------------------------------------*")
 
+class WR:
+    def __init__(self, name, team, games_played, targets, receptions, recyd, recTD,
+                attempts, rushyd, rushTD, fpoints, fppg):
+        self.name = name
+        self.team = team
+        self.attempts = attempts
+        self.rushyd = rushyd
+        self.rushTD = rushTD
+        self.targets = targets
+        self.receptions = receptions
+        self.recyd = recyd
+        self.recTD = recTD
+        self.games_played = games_played
+        self.fpoints = fpoints
+        self.fppg = fppg
+
+    def print_stats(self):
+        print("*------------------------------------*")
+        print("Name: \t\t", self.name, "\nTeam: \t\t", self.team, "\nTDs: \t\t", int(self.rushTD) + int(self.recTD),
+            "\nRec Yd: \t", self.recyd, "\nReceptions: \t", self.receptions, "\nTargets: \t", self.targets,
+            "\n", self.name, " scored ", self.fppg, " points per game")
+        print("* Fantasy Points {} *".format(self.fpoints))
+        print("*------------------------------------*")
 
 class QB:
     def __init__(self, name, team, games, completions, attempts, yards,
@@ -39,21 +67,27 @@ class QB:
         self.fpoints = fpoints
         self.fppg = fppg
 
-#    def print_stats(self):
+    def print_stats(self):
+        print("*------------------------------------*")
+        print("Name: \t\t", self.name, "\nTeam: \t\t", self.team, "\nPass TDs: \t", self.TD,
+            "\nRush Yd: \t", self.rushyd, "\nInts: \t\t", self.ints, "\n", self.name, " scored ", self.fppg, " points per game")
+        print("* Fantasy Points {} *".format(self.fpoints))
+        print("*------------------------------------*")
 
 
 team = []
 url = 'http://www.fftoday.com/stats/playerstats.php?Season=2016&GameWeek=&PosID='
 
-pos = 'RB'
-
+pos = (input("What position is the player: \n ---> "))
 if pos == 'QB':
     url += '10'
+    col_max = 12
 elif pos == 'RB':
     url += '20&LeagueID=1'
+    col_max = 11
 elif pos == 'WR':
     url += '30&LeagueID=1'
-
+    col_max = 11
 # gets webpage with stats
 page = urllib.request.urlopen(url).read()
 
@@ -62,4 +96,28 @@ col_count =0
 player_array = []
 for row in soup.find_all('td', class_='sort1'):
 #    print(row.text)
-    player_array.append(row.text)
+    if col_count == 0:
+        player_array.append(row.find('a').text)
+        col_count += 1
+    elif col_count == col_max:
+        player_array.append(row.text)
+        col_count = 0
+    else:
+        player_array.append(row.text)
+        col_count += 1
+
+if pos == 'QB':
+    player = QB(player_array[0], player_array[1], player_array[2], player_array[3],
+                player_array[4], player_array[5], player_array[6], player_array[7],
+                player_array[8], player_array[9], player_array[10], player_array[11],
+                player_array[12])
+elif pos == 'RB':
+    player = RB(player_array[0], player_array[1], player_array[2], player_array[3],
+                player_array[4], player_array[5], player_array[6], player_array[7],
+                player_array[8], player_array[9], player_array[10], player_array[11])
+else:
+    player = WR(player_array[0], player_array[1], player_array[2], player_array[3],
+                player_array[4], player_array[5], player_array[6], player_array[7],
+                player_array[8], player_array[9], player_array[10], player_array[11])
+
+player.print_stats()
